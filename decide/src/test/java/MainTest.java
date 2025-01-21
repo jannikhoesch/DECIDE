fb2f54fc79cd4a9a89eb743e632426a255b08562
Trighap52 <Trighap52@gmail.com>
2025-01-21 17:05:35 +0100

    Added Maven to the project and added Junit for tests and a simple test example Linked to issue #2

---
+++ decide/src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import com.decide.Main;
+
+class MainTest {
+    @Test
+    void testMainOutput() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        Main.main(null);
+
+        assertEquals("Hello world!" + System.lineSeparator(), outputStream.toString());
+
+        System.setOut(originalOut);
+    }
+}

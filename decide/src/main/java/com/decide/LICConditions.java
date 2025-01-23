e356f4e61c892689efb72101e9f95523f8656609
Amanda Ekholm <amanda.margareta.ekholm@gmail.com>
2025-01-23 21:00:28 +0100

    feat: implement LIC9

--- decide/src/main/java/com/decide/LICConditions.java
+++ decide/src/main/java/com/decide/LICConditions.java
@@ -24,7 +24,7 @@ public class LICConditions {
             // case 8:
             //     return LIC8(points, parameters, numPoints);
             // case 9:
-            //     return LIC9(points, parameters, numPoints);
+            //    return LIC9(Point[] points, int C_PTS, int D_PTS, double EPSILON , int numPoints);
             // case 10:
             //     return LIC10(points, parameters, numPoints);
             // case 11:
@@ -40,5 +40,32 @@ public class LICConditions {
         }
     } 
 
+    public static boolean LIC9(Point[] points, int C_PTS, int D_PTS, double EPSILON , int numPoints) {
+        if (numPoints < 5) {
+            return false;
+        }
+
+        int index = 0;
+
+        while (index + C_PTS + D_PTS < numPoints) {
+            Point A = points[index];
+            Point B = points[index + C_PTS];
+            Point C = points[index + C_PTS + D_PTS];
+
+            if ((A.x != B.x || A.y != B.y) && (C.x != B.x || C.y != B.y)) {
+                double[] BA = {A.x-B.x, A.y-B.y};
+                double[] BC = {C.x-B.x, C.y-B.y};
+                double normBA = Math.sqrt(Math.pow(BA[0], 2) + Math.pow(BA[1], 2));
+                double normBC = Math.sqrt(Math.pow(BC[0], 2) + Math.pow(BC[1], 2));
+                double BAdotBC = BA[0]*BC[0] + BA[1]*BC[1];
+                double angle = Math.acos(BAdotBC/(normBA*normBC));
+                if (angle < Math.PI - EPSILON || angle > Math.PI + EPSILON) {
+                    return true;
+                }
+            }
+            index++;
+        }
+        return false;
+    }
 
 }
--- decide/src/test/java/LICConditionsTest.java
+++ decide/src/test/java/LICConditionsTest.java
@@ -1,7 +1,56 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import com.decide.LICConditions;
+import com.decide.Point;
 
 public class LICConditionsTest {
+    @Test
+    void testLIC9() {
+        //we need at least 6 points, so this should be false
+        Point[] points = {new Point(0, 0), new Point(0, 0)};
+        int C_PTS = 2;
+        int D_PTS = 2;
+        double EPSILON = 0.14;
+        int numPoints = 2;
+
+        boolean result = LICConditions.LIC9(points, C_PTS, D_PTS, EPSILON, numPoints);
+        assertEquals(false, result, "the test should be false");
+
+
+        //the first three points should form an angle of pi/2, so this should be true
+        Point[] points2 = {new Point(1, 0), new Point(0, 0), new Point(0, 1), new Point(0, 0), new Point(0, 0), new Point(0, 0)};
+        int C_PTS2 = 1;
+        int D_PTS2 = 1;
+        double EPSILON2 = 0.14;
+        int numPoints2 = 6;
+
+        boolean result2 = LICConditions.LIC9(points2, C_PTS2, D_PTS2, EPSILON2, numPoints2);
+        assertEquals(true, result2, "the test should be true");
+
+
+        //the 3rd point is on the vertex, and no other combination should give an angle either, so this should be false
+        Point[] points3 = {new Point(1, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0)};
+        int C_PTS3 = 1;
+        int D_PTS3 = 1;
+        double EPSILON3 = 0.14;
+        int numPoints3 = 6;
+
+        boolean result3 = LICConditions.LIC9(points3, C_PTS3, D_PTS3, EPSILON3, numPoints3);
+        assertEquals(false, result3, "the test should be false");
+
+
+        //the 3 first points are on a straight line so this should be false
+        Point[] points4 = {new Point(1, 0), new Point(0, 0), new Point(-1, 0), new Point(-1, 0), new Point(0, 0), new Point(0, 0)};
+        int C_PTS4 = 1;
+        int D_PTS4 = 1;
+        double EPSILON4 = 0.14;
+        int numPoints4 = 6;
+
+        boolean result4 = LICConditions.LIC9(points4, C_PTS4, D_PTS4, EPSILON4, numPoints4);
+        assertEquals(false, result4, "the test should be false");
+    }
+
     
 }

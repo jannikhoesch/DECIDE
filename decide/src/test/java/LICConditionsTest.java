23d66378f61fe2e2c13860cbcb539fd42a6c616b
Harald Hamrin Reinhed <harald.hamrei@gmail.com>
2025-01-23 12:04:27 +0100

    Implemented point class into LIC1
    Implemented the points class that was added while I was working on this branch.

--- decide/src/test/java/LICConditionsTest.java
+++ decide/src/test/java/LICConditionsTest.java
@@ -5,56 +5,54 @@ import org.junit.jupiter.api.Test;
 
 import com.decide.LICConditions;
 import com.decide.Parameters;
+import com.decide.Point;
 
 public class LICConditionsTest {
-    
+
     // LIC1:
     @Test
     void testLIC1True() {
-        double[][] points = {{0, 0}, {10, 0}, {1, 2}, {2, 3}};
+        Point[] points = {new Point(0, 0), new Point(10, 0), new Point(1, 2), new Point(2, 3)};
         double length = 5;
         int numPoints = 4;
         assertTrue(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {10, 0}, {2, 1}};
+
+        points = new Point[]{new Point(0, 0), new Point(10, 0), new Point(2, 1)};
         length = 8;
         numPoints = 3;
         assertTrue(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {7, 1}, {3, 4}};
+
+        points = new Point[]{new Point(0, 0), new Point(7, 1), new Point(3, 4)};
         length = 6;
         numPoints = 3;
         assertTrue(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {20, 0}, {30, 0}};
+
+        points = new Point[]{new Point(0, 0), new Point(20, 0), new Point(30, 0)};
         length = 15;
         numPoints = 3;
         assertTrue(LICConditions.LIC1(points, length, numPoints));
     }
-    
-    
+
     @Test
     void testLIC1False() {
-        double[][] points = {{0, 0}, {1, 0}, {1, 2}, {2, 3}};
+        Point[] points = {new Point(0, 0), new Point(1, 0), new Point(1, 2), new Point(2, 3)};
         double length = 5;
         int numPoints = 4;
         assertFalse(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {2, 2}, {4, 4}};
+
+        points = new Point[]{new Point(0, 0), new Point(2, 2), new Point(4, 4)};
         length = 3;
         numPoints = 3;
         assertFalse(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {1, 1}, {2, 2}};
+
+        points = new Point[]{new Point(0, 0), new Point(1, 1), new Point(2, 2)};
         length = 1.5;
         numPoints = 3;
         assertFalse(LICConditions.LIC1(points, length, numPoints));
-    
-        points = new double[][]{{0, 0}, {1, 0}, {1, 1}, {2, 1}, {2, 2}};
+
+        points = new Point[]{new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1), new Point(2, 2)};
         length = 4;
         numPoints = 5;
         assertFalse(LICConditions.LIC1(points, length, numPoints));
     }
-    
-    
 }

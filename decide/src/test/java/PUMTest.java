2a2c6f92340f09fd8b343c1362e3b853696dc28c
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-01-27 15:31:42 +0100

    feat/added PUM test
    added another assertTrue in tests for PUM

--- decide/src/test/java/PUMTest.java
+++ decide/src/test/java/PUMTest.java
@@ -15,9 +15,9 @@ class PUMTest {
     void testPUMOutput() {
         boolean[] CMV = {true, false, true, false}; // Condition Met Vector
         int[][] LCM = { // Logical Connector Matrix
-            {1, 2, 777, 1},
-            {2, 1, 777, 2},
-            {777, 777, 1, 2},
+            {1, 2, 3, 1},
+            {2, 1, 3, 2},
+            {3, 3, 1, 2},
             {1, 2, 2, 1}
         };
         // Expected PUM result
@@ -30,5 +30,21 @@ class PUMTest {
 
         boolean[][] actualPUM = Decide.PUM(CMV, LCM);
         assertTrue(java.util.Arrays.deepEquals(expectedPUM, actualPUM), "PUM arrays do not match.");
+
+        boolean[] CMV1 = {true, false}; // Condition Met Vector
+        int[][] LCM1 = { // Logical Connector Matrix
+            {1, 2},
+            {2, 3},
+        };
+        // Expected PUM result
+        boolean[][] expectedPUM1 = {
+            {true, true},
+            {true,  true}
+        };
+
+        boolean[][] actualPUM1 = Decide.PUM(CMV1, LCM1);
+        assertTrue(java.util.Arrays.deepEquals(expectedPUM1, actualPUM1), "PUM arrays do not match.");
+
+
     }
 }

2f5d3e850696cc092f3af54f4f40b9c9b23f87a2
amaekh <amanda.margareta.ekholm@gmail.com>
2025-01-30 10:33:10 +0100

    fix: add two tests (#88)

--- decide/src/test/java/FUVTest.java
+++ decide/src/test/java/FUVTest.java
@@ -9,16 +9,31 @@ import com.decide.Decide;
 class FUVTest {
     @Test
     void testFUVOutput() {
+        //test 1
         boolean[][] PUM = {
-            {false, true, true},
-            {true, false, true},
-            {true, true, true},
+            {false, false},
+            {false, false},
             };
-        
-        boolean[] PUV = {false, true, true};
-
-        boolean[] expectedFUV = {true, false, true};
-
+        boolean[] PUV = {true, false};
+        boolean[] expectedFUV = {false, true};
         assertTrue(Arrays.equals(expectedFUV, Decide.FUV(PUV, PUM)));
+
+        //test 2
+        boolean[][] PUM2 = {
+            {true, true},
+            {true, true},
+            };
+        boolean[] PUV2 = {true, false};
+        boolean[] expectedFUV2 = {true, true};
+        assertTrue(Arrays.equals(expectedFUV2, Decide.FUV(PUV2, PUM2)));
+
+        //test 3
+        boolean[][] PUM3 = {
+            {true, false},
+            {true, false},
+            };
+        boolean[] PUV3 = {true, false};
+        boolean[] expectedFUV3 = {false, true};
+        assertTrue(Arrays.equals(expectedFUV3, Decide.FUV(PUV3, PUM3)));
     }
 }

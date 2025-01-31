e009d937dcb57e029d0062fe398f8dfa5303f53c
amaekh <amanda.margareta.ekholm@gmail.com>
2025-01-31 11:00:43 +0100

    doc: Add LAUNCHTest documentation (#101)
    fixes #100

--- decide/src/test/java/LAUNCHTest.java
+++ decide/src/test/java/LAUNCHTest.java
@@ -7,12 +7,14 @@ import com.decide.Decide;
 class LAUNCHTest {
     @Test
     void testLAUNCHtrue() {
+        //If all elements in FUV are true, the LAUNCH-method should return true
         boolean[] FUV = {true, true, true, true}; 
         assertTrue(Decide.LAUNCH(FUV));
     }
 
     @Test
     void testLAUNCHfalse() {
+        //If atleast one element in FUV is false, the LAUNCH-method should return false
         boolean[] FUV = {true, true, false, true}; 
         assertFalse(Decide.LAUNCH(FUV));
     }
